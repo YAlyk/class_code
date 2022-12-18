7288c3ee747a53fedf67bae641394596400314c3
Yalyk <koksharov.andrew23@yandex.ru>
2022-12-18 19:41:55 +0300

    .

--- web/90-1/les2.py
+++ web/90-1/les2.py
@@ -11,20 +11,28 @@
 #     if b == 'учёба' and a == 'плохо':
 #         print('Сочувствую')
 
-per_chislo = int(input('Введите первое число: '))
-vtor_chislo = int(input('Введите второе число: '))
-znak = input('Введите знак: ')
+# per_chislo = int(input('Введите первое число: '))
+# vtor_chislo = int(input('Введите второе число: '))
+# znak = input('Введите знак: ')
 
-if znak == '+':
-    print(per_chislo+vtor_chislo)
-elif znak == '-':
-    print(per_chislo-vtor_chislo)
-elif znak == '*':
-    print(per_chislo*vtor_chislo)
-elif znak == '/':
-    if vtor_chislo == 0:
-        print('Делить на 0 нельзя!')
-    else:
-        print(per_chislo/vtor_chislo)
+# if znak == '+':
+#     print(per_chislo+vtor_chislo)
+# elif znak == '-':
+#     print(per_chislo-vtor_chislo)
+# elif znak == '*':
+#     print(per_chislo*vtor_chislo)
+# elif znak == '/':
+#     if vtor_chislo == 0:
+#         print('Делить на 0 нельзя!')
+#     else:
+#         print(per_chislo/vtor_chislo)
+# else:
+#     print('Такого знака в этом калькуляторе нет!')
+
+a = (input(""))
+b = (input(""))
+c = (input(""))
+if (a == "красный" and b == "желтый" and c == "зеленый") or (a == '3' and b == '2' and c == '1'):
+    print("ПОЕХАЛИ")
 else:
-    print('Такого знака в этом калькуляторе нет!')
+    print("СТОИ")
