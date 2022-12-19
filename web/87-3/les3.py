b5178270c638739472109126bc70442472c51591
Yalyk <koksharov.andrew23@yandex.ru>
2022-12-19 19:05:55 +0300

    18/00

--- web/87-3/les3.py
+++ web/87-3/les3.py
@@ -1,9 +1,20 @@
-def sl_vich(a, b):
-   summ = a + b
-   vichit = a - b
-   return summ, vichit
+# def sl_vich(a, b):
+#    summ = a + b
+#    vichit = a - b
+#    print(summ, vichit)
+#    return summ, vichit
 
-a = int(input("Введите первое число: "))
-b = int(input('Введите второе число: '))
+# a = int(input("Введите первое число: "))
+# b = int(input('Введите второе число: '))
 
-print(sl_vich(a, b))
+# print(sl_vich(a, b))
+
+# b = (2, 8, 9, 9, 9)
+# c = True
+# a = ['текст', "ещё текст"]
+# print(b.count(9))
+
+# print(type(a), type(c), type(b))
+
+# a = bool(0) # a = False
+# print(a)
