f869166a3abefe629d4b328f7144b25842b5305b
Yalyk <koksharov.andrew23@yandex.ru>
2022-12-21 17:41:05 +0300

    .

--- web/24-H/lesson1.py
+++ web/24-H/lesson1.py
@@ -1,3 +1,14 @@
-print('hello')
-# комментарий
+# print('hello')
+# print('привет')
 
+# print(123)
+# print('123')
+
+# print('моё любимое число:', 23)
+
+averge = 4.81
+print('среднее значение:', averge)
+
+print('')
+averge = 'text'
+print(averge)
