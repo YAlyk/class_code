216e865682a3027717f21c4a28ec77dab6e59724
Yalyk <koksharov.andrew23@yandex.ru>
2022-12-27 16:08:44 +0300

    .

---
+++ web/103-3/les1.py
@@ -0,0 +1,32 @@
+# a = int(input('Введите число: '))
+# if a > 10:
+#     print('Вы ввели число больше десяти!')
+# elif a == 10:
+#     print('a равно 10!')
+# else:
+#     print('Вы ввели число меньше или равное десяти!')
+
+# i = 10
+# while i > 0:
+#     if i == 6:
+#         continue
+#     print(i)
+#     i-=1
+# else:
+#     print('цикл закончен')
+
+# первое задание 1 вариант
+# for i in range(0, 501, 2):
+#     print(i)
+
+# первое задание 2 вариант
+# for i in range(0, 501, 1):
+#     if i % 2 == 0:
+#         print(i)
+
+#задание 2 первый вариант
+# n = int(input('введите чило: '))
+# summa = 0
+# for i in range(0, n+1, 1):
+#     summa+=i
+# print(summa)
