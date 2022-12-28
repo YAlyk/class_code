36d908ea93fc7e697c395917812f2d54a050e2da
YAlyk <koksharov.andrew23@yandex.ru>
2022-12-28 17:42:09 +0300

    /

---
+++ web/24-H/les3.py
@@ -0,0 +1,22 @@
+# a = 10
+# while a >=0:
+#     if a == 7:
+#         continue
+#     print(a)
+#     a-=1
+      
+# else:
+#     print('цикл закончен')
+
+# a = 4
+# for i in range(5):
+#     a-=1
+# print(a)
+
+while True:
+    a = int(input('Введите число: '))
+    print(a**2)
+    otvet = input('Хотите продолжить? ').lower()
+    print(otvet)
+    if otvet == 'выйти':
+        break
