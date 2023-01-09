13596fa9e7f0437f2fcef82faed538d6c154df13
Yalyk <koksharov.andrew23@yandex.ru>
2023-01-09 18:30:07 +0300

    csv

--- web/105-1/les1.py
+++ web/105-1/les1.py
@@ -23,18 +23,43 @@
 # print('#'*max(list))
 
 
+# n = list(map(int, input().split()))
+# xmax = len(n)
+# ymax = max(n)
 
-n = list(map(int, input().split()))
-xmax = len(n)
-ymax = max(n)
+# print('*'*(xmax+2))
+# print('*'+' '*xmax+'*')
+# for y in range(1, ymax+1):
+#     print(end='*')
+#     for nk in n:
+#         if nk >= ymax-y+1:
+#             print(end='*')
+#         else:
+#             print(end=' ')
+#     print('*')
 
-print('*'*(xmax+2))
-print('*'+' '*xmax+'*')
-for y in range(1, ymax+1):
-    print(end='*')
-    for nk in n:
-        if nk >= ymax-y+1:
-            print(end='*')
-        else:
-            print(end=' ')
-    print('*')
+
+# a = {'apple', 'orange', 'mandarin', 'banan'}
+# b = {'google', 'apple', 'microsoft'}
+# # print(a ^ b)
+# a.
+
+
+# a = input().lower()
+# print(a)
+# b = input().upper()
+# print(b)
+
+alfavit = {'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.',
+           'g': '--.', 'h': '....', 'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.',
+           'o': '---', 'p': '.--.', 'q': '--.-',
+           'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-',
+           'y': '-.--', 'z': '--..'}
+text = input().lower().split()
+a = []
+for i in text:
+    print(''.join(alfavit[j] for j in i))
+
+# a = [1, 2, 3]
+# for i in a:
+#     print(i)
