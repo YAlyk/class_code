a0956f3271d6657a1bc32a063dfd70d076902187
Yalyk <koksharov.andrew23@yandex.ru>
2023-01-09 19:07:11 +0300

    /

--- web/105-1/les2.py
+++ web/105-1/les2.py
@@ -1,14 +1,16 @@
-import pandas as pd
-# my_series = pd.Series([5, 6, 7, 8, 9, 10], index=[
-#                       'a', 'b', 'c', 'd', 'e', 'f'])
-# print(my_series[['a', 'b', 'f']])
-# print(my_series.index)
-# print(my_series.values)
-df = pd.DataFrame({'coutry': [
-                  'Kazahstan', 'Russia', 'Belarus', 'Ukraine'], 'population': [17.04, 143.5, 9.5, 4.5], 'square': [2724902, 17125191, 2017600, 603628]})
-df.index = ['KZ', 'RU', 'BY', 'UA']
-df.index.name='Country code'
+# import pandas as pd
+# # my_series = pd.Series([5, 6, 7, 8, 9, 10], index=[
+# #                       'a', 'b', 'c', 'd', 'e', 'f'])
+# # print(my_series[['a', 'b', 'f']])
+# # print(my_series.index)
+# # print(my_series.values)
+# df = pd.DataFrame({'coutry': [
+#                   'Kazahstan', 'Russia', 'Belarus', 'Ukraine'], 'population': [17.04, 143.5, 9.5, 4.5], 'square': [2724902, 17125191, 2017600, 603628]})
+# df.index = ['KZ', 'RU', 'BY', 'UA']
+# df.index.name='Country code'
 
-# print(df.loc['KZ'])
+# # print(df.loc['KZ'])
 
-df.to_csv('coutry.csv', sep=',')
+# # df.to_csv('coutry.csv', sep=',')
+# print(df)
+
