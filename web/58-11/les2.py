b8acf844a1acdcffab0a2a8607987a64f02e564e
Yalyk <koksharov.andrew23@yandex.ru>
2023-01-17 19:48:44 +0300

    .

--- web/58-11/les2.py
+++ web/58-11/les2.py
@@ -80,9 +80,47 @@
 # plt.show()
 
 
-import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot([0,1,2,3,4],[0,6,7,15,19])
-ax.scatter([0,1,2,3,4], [1,3,8,12,27])
-plt.show()
+# import matplotlib.pyplot as plt
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.plot([0, 1, 2, 3, 4], [0, 6, 7, 15, 19], color='red', linewidth=5)
+# ax.scatter([0,1,2,3,4], [1,3,8,12,27], color='blue', marker='*')
+# plt.show()
+
+# import matplotlib.pyplot as plt
+# plt.plot([0, 1, 2, 3, 4], [0, 6, 7, 15, 19], color='red', linewidth=2)
+# plt.scatter([0, 1, 2, 3, 4], [1, 3, 8, 12, 27], color='blue', marker='*')
+
+# plt.show()
+
+
+# import matplotlib.pyplot as plt
+# fig = plt.figure()
+# ax_1 = fig.add_subplot(2, 2, 1)
+# ax_2 = fig.add_subplot(2, 2, 2)
+# ax_3 = fig.add_subplot(2, 2, 3)
+# ax_4 = fig.add_subplot(2, 2, 4)
+
+# ax_1.set(title='ax_1', xticks=[], yticks=[])
+# ax_2.set(title='ax_2', xticks=[], yticks=[])
+# ax_3.set(title='ax_3', xticks=[], yticks=[])
+# ax_4.set(title='ax_4', xticks=[], yticks=[])
+
+# plt.show()
+
+
+# import matplotlib.pyplot as plt
+
+# fig = plt.figure()
+#         кол-во строк # стобцов # индекс # ячейки
+# ax_1 = fig.add_subplot(2, 3, 1)
+# ax_2 = fig.add_subplot(2, 3, 3)
+# ax_3 = fig.add_subplot(2, 3, 5)
+
+# ax_1.set(title='ax_1', xticks=[], yticks=[])
+# ax_2.set(title='ax_2', xticks=[], yticks=[])
+# ax_3.set(title='ax_3', xticks=[], yticks=[])
+
+# plt.show()
+
+
