6f752ac570173bf348ad9f397af3b27a65ed1442
Yalyk <koksharov.andrew23@yandex.ru>
2023-01-31 19:04:08 +0300

    \

---
+++ web/71-1/71-1-2/les1.py
@@ -0,0 +1,18 @@
+import random
+
+chislo = random.randint(1, 100)
+otvet = False
+att = 0
+while otvet !=True:
+    otvet_user = int(input('Введите число: '))
+    if otvet_user == chislo:
+        print('Вы угадали! Поздравляем! Количество попыток:', att)
+        otvet = True
+    elif otvet_user > chislo:
+        print('-')
+        att+=1
+    elif otvet_user < chislo:
+        print('+')
+        att += 1
+    
+
