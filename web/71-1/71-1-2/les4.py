987612e85255da36d620444cb8e4c31ee66c1521
Yalyk <koksharov.andrew23@yandex.ru>
2023-02-09 19:48:05 +0300

    tk

--- web/71-1/71-1-2/les4.py
+++ web/71-1/71-1-2/les4.py
@@ -186,3 +186,25 @@
 #                  padx=4, pady=4, sticky=NSEW)
 
 # root.mainloop()
+
+
+from tkinter import *
+
+top = Tk()
+top.geometry('200x100')
+
+def select():
+    sel = 'Value = '+str(v.get())
+    label.config(text=sel)
+
+v = DoubleVar()
+scale = Scale(top, variable=v, from_=1, to=50, orient=HORIZONTAL)
+scale.pack(anchor=CENTER)
+
+btn = Button(top, text='Value', command=select)
+btn.pack(anchor=CENTER)
+
+label = Label(top)
+label.pack()
+
+top.mainloop()
