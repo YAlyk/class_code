495f49ba1d684cad80bef463f3629ab7941acd4d
Yalyk <koksharov.andrew23@yandex.ru>
2023-03-09 12:47:16 +0300

    /

--- web/77-1/les1.py
+++ web/77-1/les1.py
@@ -15,3 +15,10 @@
 
 import sqlite3
 
+conn = sqlite3.connect('test.db')
+
+cursor = conn.cursor()
+# cursor.execute("""CREATE TABLE albums
+# (title text, artist text, release_date text, publisher text, media_type text)  """)
+
+cursor.commit()
