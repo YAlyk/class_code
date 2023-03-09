3f4fc28796727a83958e34f514c53c8988aa5b98
Yalyk <koksharov.andrew23@yandex.ru>
2023-03-09 12:21:06 +0300

    /

---
+++ web/77-1/les1.py
@@ -0,0 +1,17 @@
+# f = lambda x: x * x
+# print(type(f))
+
+
+# def func(x):
+#     return x**2
+
+# a = [1, 5, 6]  # список list
+# a = (8, 6, 3)  # кортеж tuple
+# a = {8, 8, 9, 6, 2, 5, 88, 8}  # множество set
+# a = {'a': 5, 'b': 10}  # словарик dict
+
+# print(a)
+
+
+import sqlite3
+
