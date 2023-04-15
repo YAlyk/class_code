90fdf7dd26b28e8583b4fc04e00725ad77d8b988
Yalyk <koksharov.andrew23@yandex.ru>
2023-04-15 16:46:17 +0300

    /

--- web/84-1/zamena.py
+++ web/84-1/zamena.py
@@ -13,16 +13,16 @@
 
 # print(c)
 
-def pw(a):
-    return a**2
+# def pw(a):
+#     return a**2
 
 
-a = [1, 5, 9, 6, 8]
-print(list(map(float, a)))
+# a = [1, 5, 9, 6, 8]
+# print(list(map(float, a)))
 
 
-a = [1, 5, 9, 6, 8]
-print(list(map(lambda x: x**2, a)))
+# a = [1, 5, 9, 6, 8]
+# print(list(map(lambda x: x**2, a)))
 
 
 # old_list = [1, 5, 9, 6, 8]
@@ -30,3 +30,33 @@ print(list(map(lambda x: x**2, a)))
 # for item in old_list:
 #     new_list.append(pw(item))
 # print(new_list)
+
+
+# def func(elem):
+#     return elem > 0
+
+
+# numbers = [-1, 2, -3, 4, 0, -20, 10]
+# positive_numbers = list(filter(func, numbers))
+# print(positive_numbers)
+
+# map - работает со всеми функциями (базовыми и рукописными)
+#filter - работает только с функциями, которые возвращают bool type
+
+
+def func(a, b):
+    s = a+b
+    if s % 10 > 5:
+        return 'a'
+    elif s % 10 <= 5:
+        return 'b'
+
+
+x = [5, 7, 3, 6, 8, 0, 3, 4, 7, 9, 0, 2]
+y = [9, 6, 3, 6, 8, 3, 6, 8, 4, 5, 7, 4]
+res = []
+for i in range(len(x)):
+    res.append(func(x[i], y[i]))
+print(res)
+
+res = list(map(lambda a,b: 'a' if (a+b)%10>65 else 'b', x,y))
