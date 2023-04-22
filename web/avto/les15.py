d2a177e1b2de97bf0b6cb3bd7c2734112d2a8e77
Yalyk <koksharov.andrew23@yandex.ru>
2023-04-22 13:44:56 +0300

    /

---
+++ web/avto/les15.py
@@ -0,0 +1,30 @@
+""" тип документация"""
+import platform
+""" тип документация"""
+
+
+class CarClass:
+    """Наш класс"""
+
+    def __init__(self, color, make, model, year):
+        """Constructor"""
+        self.color = color
+        self.make = make
+        self.model = model
+        self.year = year
+
+        if "Windows" in platform.platform():
+            print("You're using Windows!")
+
+        self.weight = self.get_weight()
+        self.model = self.new_model("suzuki")
+
+    def get_weight(self):
+        """получить значения веса"""
+        self.weight = "2000 lbs"
+        return self.weight
+
+    def new_model(self, mod):
+        """обновление модели"""
+        self.model = mod
+        return self.model
