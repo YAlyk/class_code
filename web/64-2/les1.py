7ee0e2c03a4acdd66a91ad7747014912492b9263
Yalyk <koksharov.andrew23@yandex.ru>
2023-04-24 20:09:08 +0300

    /

--- web/64-2/les1.py
+++ web/64-2/les1.py
@@ -1,14 +1,17 @@
 import random
 from collections import Counter
 
+
 def data_generate():
     random.seed(100)
-    data = [random.randint(0,30) for el in range(1000)]
+    data = [random.randint(0, 30) for el in range(1000)]
 
     return data
 
+
 def averge_fu(data):
     return round(sum(data)/len(data), 3)
+
 
 def mode(data):
     c = Counter(data)
@@ -23,7 +26,22 @@ def median(data):
     else:
         return sum(sorted(data)[index-1:index+1]) / 2
 
+
+def sigma(data):
+    summa = 0
+    av = averge_fu(data)
+    for el in data:
+        summa += (el-av)**2
+    return round((summa/len(data)-1) ** 0.5, 3)
+
+
+def rang(data):
+    mi = min(data)
+    ma = max(data)
+    return ma-mi
+
 # тернарная операция
+
 
 # a if <условие> else b
 # if <условие>:
@@ -34,3 +52,5 @@ data = data_generate()
 print('среднее значение:', averge_fu(data))
 print('мода:', mode(data))
 print('медиана:', median(data))
+print('среднеквадратичное отклонение:', sigma(data))
+print('размах:', rang(data))
