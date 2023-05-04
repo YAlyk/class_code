9f82234a14179b0a8fb102eaac82bd93736716f1
Yalyk <koksharov.andrew23@yandex.ru>
2023-05-04 19:47:18 +0300

    .

--- web/64-2/funct.py
+++ web/64-2/funct.py
@@ -1,9 +1,15 @@
+"""
+это модуль для функций
+"""
 import matplotlib.pyplot as plt
 import random
 from collections import Counter
 
 
 def averge_fu(data):
+    """
+    выводит среднее значение
+    """
     return round(sum(data)/len(data), 3)
 
 
@@ -55,6 +61,11 @@ def median(data: list) -> float:
 
 
 def get_data_from_file() -> tuple:
+    """
+    это функция для чтения файла
+    ничего передавать не надо
+    выводит все данные файла
+    """
     file = open(r'web\64-2\lesson_4.txt', 'r')
     file = [line.strip() for line in file]
 
@@ -151,56 +162,3 @@ def get_line(data_x, data_y):
     y.append(k*x_max+b)
 
     return x, y, k, b
-
-
-m_h, m_w, f_h, f_w = get_data_from_file()
-x_m_h, y_m_h = data_to_bar(m_h)
-x_m_w, y_m_w = data_to_bar(m_w)
-x_f_h, y_f_h = data_to_bar(f_h)
-x_f_w, y_f_w = data_to_bar(f_h)
-m_l_x, m_l_y, m_k, m_b = get_line(m_w, m_h)
-f_l_x, f_l_y, f_k, f_b = get_line(f_w, f_h)
-
-print('правило трёх сигм')
-print('рост мужчины', percent_sigma(m_h))
-print('вес мужчины', percent_sigma(m_w))
-print('рост женщины', percent_sigma(f_h))
-print('вес женщины', percent_sigma(f_w))
-
-print()
-print('Статистика по мужчинам. Два числа - сначала для роста, потом для веса')
-print('среднее значение', averge_fu(m_h), averge_fu(m_w))
-print('стандартное отклонение', sigma(m_h), sigma(m_w))
-print('минимальное значение', min(m_h), min(m_w))
-print('первый квартиль', kvartil_25(m_h), kvartil_25(m_w))
-print('медиана/2 квартиль', median(m_h), median(m_w))
-print('третий квартиль', kvartil_75(m_h), kvartil_75(m_w))
-print('максимальное значение', max(m_h), max(m_w))
-print()
-print('Статистика по женщинам. Два числа - сначала для роста, потом для веса')
-print('среднее значение', averge_fu(f_h), averge_fu(f_w))
-print('стандартное отклонение', sigma(f_h), sigma(f_w))
-print('минимальное значение', min(f_h), min(f_w))
-print('первый квартиль', kvartil_25(f_h), kvartil_25(f_w))
-print('медиана/2 квартиль', median(f_h), median(f_w))
-print('третий квартиль', kvartil_75(f_h), kvartil_75(f_w))
-print('максимальное значение', max(f_h), max(f_w))
-
-fig, axes = plt.subplots(1, 2)
-
-print()
-print('коэффициент корреляции для мужчин', corr_coef(m_h, m_w))
-print('коэффициент корреляции для женщин', corr_coef(f_h, f_w))
-print('коэффициент детерминации для мужчин', round(corr_coef(m_h, m_w)**2, 3))
-print('коэффициент детерминации для женщин', round(corr_coef(f_h, f_w)**2, 3))
-axes[0].scatter(m_w, m_h, color='blue')
-axes[0].plot(m_l_x, m_l_y, color='red')
-axes[1].scatter(f_w, f_h, color='red')
-axes[1].plot(f_l_x,f_l_y, color='blue')
-
-# fig, axes = plt.subplots(2, 2)
-# axes[0, 0].bar(x_m_h, y_m_h, color='red')
-# axes[0, 1].bar(x_m_w, y_m_w, color='blue')
-# axes[1, 0].bar(x_f_h, y_f_h, color='red')
-# axes[1, 1].bar(x_f_w, y_f_w, color='blue')
-plt.show()
---
+++ web/64-2/les4.py
@@ -0,0 +1,4 @@
+import funct as t
+
+a = [1,2,3,4,5,6]
+print(t.averge_fu(a))
