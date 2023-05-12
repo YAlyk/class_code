4720b1f247ace0cd98bd78eff8af31bb186b4955
Yalyk <koksharov.andrew23@yandex.ru>
2023-05-12 15:06:20 +0300

    /

--- web/avto/les17.py
+++ web/avto/les17.py
@@ -32,21 +32,21 @@
 # ax.plot(x, y)
 # plt.show()
 
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
-# Создаём экземпляр класса figure и добавляем к Figure область Axes
-fig, ax = plt.subplots()
-# Добавим заголовок графика
-ax.set_title('График функции')
-# Название оси X:
-ax.set_xlabel('x')
-# Название оси Y:
-ax.set_ylabel('y')
-# Начало и конец изменения значения X, разбитое на 100 точек
-x = np.linspace(-5, 5, 100) # X от -5 до 5
-# Построение прямой
-y = np.exp(x)
-# Вывод графика
-ax.plot(x, y)
-plt.show()
+# # Создаём экземпляр класса figure и добавляем к Figure область Axes
+# fig, ax = plt.subplots()
+# # Добавим заголовок графика
+# ax.set_title('График функции')
+# # Название оси X:
+# ax.set_xlabel('x')
+# # Название оси Y:
+# ax.set_ylabel('y')
+# # Начало и конец изменения значения X, разбитое на 100 точек
+# x = np.linspace(-5, 5, 100) # X от -5 до 5
+# # Построение прямой
+# y = np.exp(x)
+# # Вывод графика
+# ax.plot(x, y)
+# plt.show()
